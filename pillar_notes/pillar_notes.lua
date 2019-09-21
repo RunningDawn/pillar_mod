eefaaf1d796baa0498fe6a03bf074666e18d1f6f
Summersisle <25886892+Summersisle@users.noreply.github.com>
2019-09-21 03:35:23 -0700

    Update pillar_notes.lua

--- pillar_notes/pillar_notes.lua
+++ pillar_notes/pillar_notes.lua
@@ -4,12 +4,6 @@ end
  function pillar_notes_OnEvent(self, event, ...) 
      if event == "ADDON_LOADED" and ... == "pillar_notes" then
         self:UnregisterEvent("ADDON_LOADED")		
-	    pillar_notes:SetSize(100, 50)
-            pillar_notes:SetPoint("TOP", "Minimap", "BOTTOM", 5, -5)
-    	    pillar_notes:SetScript("OnUpdate", UpdateCoordinates)
-	    local coordsFont =    pillar_notes:CreateFontString("eCoordinatesFontString", "ARTWORK", "GameFontNormal")
- 	    pillar_notes:SetPoint("CENTER", "eCoordinates", "CENTER", 0, 0)
-	    coordsFont:Show()
- 	    eCoordinates:Show()		
+	    Pillar_Notes_Frame:Show()
 	end
 end
